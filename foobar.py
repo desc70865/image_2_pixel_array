56e4446aea924b3847a663317d228519c9b620ed
desc_inno <17777904+desc70865@users.noreply.github.com>
2020-04-19 18:12:44 +0800

    Create foobar.py

---
+++ foobar.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+from PIL import Image
+import os
+#python 2.7
+#using Pillow
+#End
+path = "."
+dirs = os.listdir(path)
+
+for i in dirs:
+	if os.path.splitext(i)[1] == ".jpg":
+		imload = Image.open(i);
+		im = imload.convert("RGB")
+		
+		width = im.size[0] / 4
+		height = im.size[1] / 4
+		demo = open('Output.txt','a');
+
+		for y in range(height):
+			for x in range(width):
+				r,g,b = im.getpixel((4*x,4*y))
+				rgb =hex(r)+''+hex(g)+''+hex(b)
+				demo.write('\t' + str(rgb))
+			demo.write("\n")
+		demo.write("End\n")
+		demo.close()
